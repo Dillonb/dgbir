3d4bb242cdf68f85361c6f3e39735b0d7052ab03
Dillon Beliveau <dillon@dgb.sh>
2025-04-20 14:48:51 -0700

    cargo fmt

--- src/ir_interpreter.rs
+++ src/ir_interpreter.rs
@@ -181,27 +181,36 @@ fn evaluate_instr(
 
             return vec![];
         }
-        crate::ir::InstructionType::ConditionalBranch => todo!("Evaluate conditional branch instruction in IR interpreter"),
-        crate::ir::InstructionType::Compare => todo!("Evaluate compare instruction in IR interpreter"),
+        crate::ir::InstructionType::ConditionalBranch => {
+            todo!("Evaluate conditional branch instruction in IR interpreter")
+        }
+        crate::ir::InstructionType::Compare => {
+            todo!("Evaluate compare instruction in IR interpreter")
+        }
         crate::ir::InstructionType::Phi => {
             // Try to find the last executed instruction
-            let last_executed = instruction.inputs.iter().filter(|input| {
-                match input {
+            let last_executed = instruction
+                .inputs
+                .iter()
+                .filter(|input| match input {
                     InputSlot::Constant(_) => false,
-                    InputSlot::InstructionOutput { instruction_index, .. } => {
-                        instruction_last_executed[*instruction_index] > 0
-                    }
-                }
-            }).last()
-            // If no instructions were executed, look for a constant
-            .unwrap_or_else(|| {
-                instruction.inputs.iter().filter(|input| {
-                    match input {
-                        InputSlot::Constant(_) => true,
-                        InputSlot::InstructionOutput { .. } => false,
-                    }
-                }).last().unwrap()
-            });
+                    InputSlot::InstructionOutput {
+                        instruction_index, ..
+                    } => instruction_last_executed[*instruction_index] > 0,
+                })
+                .last()
+                // If no instructions were executed, look for a constant
+                .unwrap_or_else(|| {
+                    instruction
+                        .inputs
+                        .iter()
+                        .filter(|input| match input {
+                            InputSlot::Constant(_) => true,
+                            InputSlot::InstructionOutput { .. } => false,
+                        })
+                        .last()
+                        .unwrap()
+                });
 
             return get_constant_inputs(&vec![*last_executed], results);
         }
@@ -220,7 +229,12 @@ pub fn interpret_block(block: &IRBlock) {
         let instruction = &block.instructions[pc];
         let instruction_index = pc;
         pc += 1;
-        results[instruction_index] = evaluate_instr(block, &instruction_last_executed, &mut results, &instruction.instruction);
+        results[instruction_index] = evaluate_instr(
+            block,
+            &instruction_last_executed,
+            &mut results,
+            &instruction.instruction,
+        );
 
         // Data for phi nodes
         instructions_executed += 1;
