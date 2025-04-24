9887c26827a1cac0e65a93eddc5118c2d77a0b2f
Dillon Beliveau <dillon@dgb.sh>
2025-04-23 17:42:18 -0700

    Move ir display traits into submodule

---
+++ src/ir/ir_display.rs
@@ -0,0 +1,115 @@
+use std::fmt::Display;
+
+use super::*;
+
+impl Display for InputSlot {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            InputSlot::InstructionOutput { block_index, instruction_index, output_index, .. } => {
+                if *output_index > 0 {
+                    write!(f, "b{}v{}_{}", block_index, instruction_index, output_index)
+                } else {
+                    write!(f, "b{}v{}", block_index, instruction_index)
+                }
+            },
+            InputSlot::BlockInput { block_index, input_index, .. } => write!(f, "b{}i{}", block_index, input_index),
+            InputSlot::Constant(constant) => write!(f, "{:?}", constant),
+        }
+    }
+}
+
+impl Display for BlockReference {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let inputs = self
+            .arguments
+            .iter()
+            .map(|input| format!("{input}"))
+            .collect::<Vec<String>>()
+            .join(", ");
+
+        write!(f, "block_{}({})", self.block_index, inputs)
+    }
+}
+
+impl Display for IndexedInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match &self.instruction {
+            Instruction::Instruction { tp, inputs, outputs } => {
+                let outputs = outputs
+                    .iter()
+                    .enumerate()
+                    .map(|(i, output)| {
+                        if i > 0 {
+                            format!("b{}v{}_{} : {:?}", self.block_index, self.index, i, output.tp)
+                        } else {
+                            format!("b{}v{} : {:?}", self.block_index, self.index, output.tp)
+                        }
+                    })
+                    .collect::<Vec<String>>()
+                    .join(", ");
+
+                let inputs = inputs
+                    .iter()
+                    .map(|input| format!("{input}"))
+                    .collect::<Vec<String>>()
+                    .join(", ");
+
+                if outputs.is_empty() {
+                    format!("{tp:?}({inputs})")
+                } else {
+                    format!("{outputs} = {tp:?}({inputs})")
+                }
+            },
+            Instruction::Branch { cond, if_true, if_false } => {
+                format!("Branch({} ? {} : {})", cond, if_true, if_false)
+            },
+            Instruction::Jump { target } => {
+                format!("Jump {target}")
+            },
+            Instruction::Return { value } => {
+                match value {
+                    Some(value) => format!("Return {value}"),
+                    None => "Return".to_string(),
+                }
+            },
+        })
+    }
+}
+
+impl Display for IRBasicBlock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+
+        let block_name = format!("block_{}", self.index);
+        let inputs = self
+            .inputs
+            .iter()
+            .enumerate()
+            .map(|(i, tp)| {
+                format!("b{}i{}: {:?}", self.index, i, tp)
+            })
+            .collect::<Vec<String>>()
+            .join(", ");
+
+
+        let instructions = self
+            .instructions
+            .iter()
+            .map(|instruction| format!("  {instruction}"))
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        write!(f, "{block_name}({inputs}):\n{instructions}")
+    }
+}
+
+impl Display for IRFunction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.blocks
+            .iter()
+            .map(|block| format!("{}", block))
+            .collect::<Vec<String>>()
+            .join("\n\n")
+            .fmt(f)
+    }
+}
+
