0bcdfee18a6bab36cf3e14881fba3d7c358ad49b
Dillon Beliveau <dillon@dgb.sh>
2025-05-11 14:53:44 -0700

    cleanup compiler a bit

--- src/compiler_aarch64.rs
+++ src/compiler_aarch64.rs
@@ -1,25 +1,16 @@
 use std::{collections::HashMap, mem};
 
-use crate::{disassembler::disassemble, ir::{Constant, DataType, IRFunction, InputSlot}, register_allocator::{alloc_for, Register, Value}};
+use crate::{disassembler::disassemble, ir::{Constant, DataType, IRFunction, InputSlot, OutputSlot}, register_allocator::{alloc_for, Register, Value}};
 use dynasmrt::{dynasm, DynasmApi, DynasmLabelApi};
-use itertools::Either;
 
-fn input_slot_to_imm_or_reg(s : InputSlot, func: &IRFunction, allocations: &HashMap<Value, Register>) -> Either<Constant, Register> {
-    match s {
-        InputSlot::InstructionOutput { .. } => Either::Right(allocations[&s.to_value(func).unwrap()]),
-        InputSlot::BlockInput { .. } => Either::Right(allocations[&s.to_value(func).unwrap()]),
-        InputSlot::Constant(constant) => Either::Left(constant),
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum ConstOrReg {
     U32(u32),
     U64(u64),
     GPR(u32),
 }
 
-fn input_slot_to_imm_or_reg_2(s : InputSlot, func: &IRFunction, allocations: &HashMap<Value, Register>) -> ConstOrReg {
+fn input_slot_to_imm_or_reg(s : InputSlot, func: &IRFunction, allocations: &HashMap<Value, Register>) -> ConstOrReg {
     match s {
         InputSlot::InstructionOutput { .. } => {
             match allocations[&s.to_value(func).unwrap()] {
@@ -88,10 +79,105 @@ fn load_32_bit_constant(ops: &mut dynasmrt::aarch64::Assembler, reg: u32, value:
     }
 }
 
-fn add_constants(a: Constant, b: Constant) -> u32 {
-    match (a, b) {
-        (Constant::U32(a), Constant::U32(b)) => a + b,
-        _ => todo!("Adding constants other than u32")
+fn compile_add(ops: &mut dynasmrt::aarch64::Assembler, func: &IRFunction, allocations: &HashMap<Value, Register>, inputs: &Vec<InputSlot>, outputs: &Vec<OutputSlot>, output_registers: Vec<Option<&Register>>) {
+    assert_eq!(inputs.len(), 2);
+    assert_eq!(outputs.len(), 1);
+    assert_eq!(output_registers.len(), 1);
+
+    let a = input_slot_to_imm_or_reg(inputs[0], func, &allocations);
+    let b = input_slot_to_imm_or_reg(inputs[1], func, &allocations);
+
+    let tp = outputs[0].tp;
+
+    let r_out = *output_registers[0].unwrap();
+
+    match (tp, r_out, a, b) {
+        (DataType::U32, Register::GPR(r_out), ConstOrReg::U32(c1), ConstOrReg::U32(c2)) => {
+            load_32_bit_constant(ops, r_out as u32, c1 + c2);
+        },
+        (DataType::U32, Register::GPR(r_out), ConstOrReg::GPR(r), ConstOrReg::U32(c)) => {
+            if c < 4096 {
+                dynasm!(ops
+                    ; add WSP(r_out as u32), WSP(r), c
+                )
+            } else {
+                println!("TODO: allocate these temporary registers in a real/safe way");
+                load_32_bit_constant(ops, 0, c);
+                dynasm!(ops
+                    ; add W(r_out as u32), W(r), W(0)
+                )
+            }
+        },
+        (DataType::U32, Register::GPR(r_out), ConstOrReg::GPR(r1), ConstOrReg::GPR(r2)) => {
+            dynasm!(ops
+                ; add W(r_out as u32), W(r1), W(r2)
+            )
+        },
+        _ => todo!("Unsupported Add operation: {:?} + {:?} with type {:?}", a, b, tp),
+    }
+}
+
+fn compile_write_ptr(ops: &mut dynasmrt::aarch64::Assembler, func: &IRFunction, allocations: &HashMap<Value, Register>, inputs: &Vec<InputSlot>) {
+    assert_eq!(inputs.len(), 3);
+    // ptr, value, type
+    let ptr = input_slot_to_imm_or_reg(inputs[0], func, &allocations);
+    let value = input_slot_to_imm_or_reg(inputs[1], func, &allocations);
+    if let InputSlot::Constant(Constant::DataType(tp)) = inputs[2] {
+        match (&ptr, &value, tp) {
+            (ConstOrReg::U64(ptr), ConstOrReg::U32(value), DataType::U32) => {
+                println!("TODO: allocate these temporary registers in a real/safe way");
+                load_64_bit_constant(ops, 0, *ptr);
+                load_32_bit_constant(ops, 1, *value);
+                dynasm!(ops
+                ; str W(1), [X(0)]
+            )
+            },
+            (ConstOrReg::U64(ptr), ConstOrReg::GPR(value), DataType::U32) => {
+                println!("TODO: allocate these temporary registers in a real/safe way");
+                load_64_bit_constant(ops, 0, *ptr);
+                dynasm!(ops
+                ; str W((*value) as u32), [X(0)]
+            )
+            },
+            (ConstOrReg::GPR(_), ConstOrReg::U32(_), DataType::U32) => todo!(),
+            (ConstOrReg::GPR(_), ConstOrReg::GPR(_), DataType::U32) => todo!(),
+            _ => todo!("Unsupported WritePtr operation: {:?} = {:?} with type {}", ptr, value, tp),
+        }
+    } else {
+        panic!("Expected a datatype constant as the third input to WritePtr");
+    }
+}
+
+fn compile_spill_to_stack(ops: &mut dynasmrt::aarch64::Assembler, func: &IRFunction, allocations: &HashMap<Value, Register>, inputs: &Vec<InputSlot>) {
+    let to_spill = input_slot_to_imm_or_reg(inputs[0], func, &allocations);
+    let stack_offset = input_slot_to_imm_or_reg(inputs[1], func, &allocations);
+    if let InputSlot::Constant(Constant::DataType(tp)) = inputs[2] {
+        match (&to_spill, &stack_offset, tp) {
+            (ConstOrReg::GPR(r), ConstOrReg::U64(offset), DataType::U32) => {
+                dynasm!(ops
+                ; str W(*r), [sp, func.get_stack_offset_for_location(*offset, DataType::U32)]
+            )
+            },
+            _ => todo!("Unsupported SpillToStack operation: {:?} to offset {:?} with datatype {}", to_spill, stack_offset, tp),
+        }
+    } else {
+        panic!("Expected a datatype constant as the third input to SpillToStack");
+    }
+}
+
+fn compile_load_from_stack(ops: &mut dynasmrt::aarch64::Assembler, func: &IRFunction, allocations: &HashMap<Value, Register>, inputs: &Vec<InputSlot>, outputs: &Vec<OutputSlot>, output_registers: Vec<Option<&Register>>) {
+    let stack_offset = input_slot_to_imm_or_reg(inputs[0], func, &allocations);
+
+    let r_out = *output_registers[0].unwrap();
+    let tp = outputs[0].tp;
+
+    match (r_out, &stack_offset, tp) {
+        (Register::GPR(r_out), ConstOrReg::U64(offset), DataType::U32) => {
+            dynasm!(ops
+            ; ldr W(r_out as u32), [sp, func.get_stack_offset_for_location(*offset, DataType::U32)]
+        )
+        },
+        _ => todo!("Unsupported LoadFromStack operation: load {} from offset {:?} with datatype {}", r_out, stack_offset, tp),
     }
 }
 
@@ -132,119 +218,28 @@ pub fn compile(func: &mut IRFunction) {
 
             match &instruction.instruction {
                 crate::ir::Instruction::Instruction { tp, inputs, outputs } => {
-                    match tp {
-                        crate::ir::InstructionType::Add => {
-                            assert_eq!(inputs.len(), 2);
-
-                            let a = input_slot_to_imm_or_reg(inputs[0], func, &allocations);
-                            let b = input_slot_to_imm_or_reg(inputs[1], func, &allocations);
-
-                            let r_out = allocations[&Value::InstructionOutput {
+                    let output_registers = outputs
+                        .iter()
+                        .enumerate()
+                        .map(|(output_index, output)| {
+                            allocations.get(&Value::InstructionOutput {
                                 instruction_index: *instruction_index,
-                                output_index: 0,
+                                output_index,
                                 block_index,
-                                data_type: outputs[0].tp,
-                            }];
+                                data_type: output.tp,
+                            })
+                        }).collect::<Vec<_>>();
 
-
-                            match (r_out, a, b) {
-                                (Register::GPR(r_out), Either::Left(c), Either::Left(c2)) => {
-                                    dynasm!(ops
-                                        ; mov X(r_out as u32), add_constants(c, c2).into()
-                                    )
-                                },
-                                (Register::GPR(r_out), Either::Left(Constant::U32(c)), Either::Right(Register::GPR(r))) => {
-                                    dynasm!(ops
-                                        ; mov X(r_out as u32), c.into()
-                                        ; add X(r_out as u32), X(r_out as u32), X(r as u32)
-                                    )
-                                },
-                                (Register::GPR(r_out), Either::Right(Register::GPR(r)), Either::Left(Constant::U32(c))) => {
-                                    dynasm!(ops
-                                        ; mov X(r_out as u32), c.into()
-                                        ; add X(r_out as u32), X(r_out as u32), X(r as u32)
-                                    )
-                                },
-                                (Register::GPR(r_out), Either::Right(Register::GPR(r1)), Either::Right(Register::GPR(r2))) => {
-                                    dynasm!(ops
-                                        ; add X(r_out as u32), X(r1 as u32), X(r2 as u32)
-                                    )
-                                }
-                                _ => todo!("Unsupported add operation: {:?} + {:?}", a, b),
-                            }
-                        },
+                    match tp {
+                        crate::ir::InstructionType::Add => compile_add(&mut ops, func, &allocations, inputs, outputs, output_registers),
                         crate::ir::InstructionType::Compare => todo!("compare"),
                         crate::ir::InstructionType::LoadPtr => todo!("load_ptr"),
-                        crate::ir::InstructionType::WritePtr => {
-                            assert_eq!(inputs.len(), 3);
-                            // ptr, value, type
-                            let ptr = input_slot_to_imm_or_reg_2(inputs[0], func, &allocations);
-                            let value = input_slot_to_imm_or_reg_2(inputs[1], func, &allocations);
-                            if let InputSlot::Constant(Constant::DataType(tp)) = inputs[2] {
-                                match (&ptr, &value, tp) {
-                                    (ConstOrReg::U64(ptr), ConstOrReg::U32(value), DataType::U32) => {
-                                        println!("TODO: allocate these temporary registers in a real/safe way");
-                                        load_64_bit_constant(&mut ops, 0, *ptr);
-                                        load_32_bit_constant(&mut ops, 1, *value);
-                                        dynasm!(ops
-                                            ; str W(1), [X(0)]
-                                        )
-                                    },
-                                    (ConstOrReg::U64(ptr), ConstOrReg::GPR(value), DataType::U32) => {
-                                        println!("TODO: allocate these temporary registers in a real/safe way");
-                                        load_64_bit_constant(&mut ops, 0, *ptr);
-                                        dynasm!(ops
-                                            ; str W((*value) as u32), [X(0)]
-                                        )
-                                    },
-                                    (ConstOrReg::GPR(ptr), ConstOrReg::U32(value), DataType::U32) => todo!(),
-                                    (ConstOrReg::GPR(ptr), ConstOrReg::GPR(value), DataType::U32) => todo!(),
-                                    _ => todo!("Unsupported WritePtr operation: {:?} = {:?} with type {}", ptr, value, tp),
-                                }
-                            } else {
-                                panic!("Expected a datatype constant as the third input to WritePtr");
-                            }
-                        },
-                        crate::ir::InstructionType::SpillToStack => {
-                            let to_spill = input_slot_to_imm_or_reg_2(inputs[0], func, &allocations);
-                            let stack_offset = input_slot_to_imm_or_reg_2(inputs[1], func, &allocations);
-                            if let InputSlot::Constant(Constant::DataType(tp)) = inputs[2] {
-                                match (&to_spill, &stack_offset, tp) {
-                                    (ConstOrReg::GPR(r), ConstOrReg::U64(offset), DataType::U32) => {
-                                        dynasm!(ops
-                                            ; str W(*r), [sp, func.get_stack_offset_for_location(*offset, DataType::U32)]
-                                        )
-                                    },
-                                    _ => todo!("Unsupported SpillToStack operation: {:?} to offset {:?} with datatype {}", to_spill, stack_offset, tp),
-                                }
-                            } else {
-                                panic!("Expected a datatype constant as the third input to SpillToStack");
-                            }
-                        },
-                        crate::ir::InstructionType::LoadFromStack => {
-                            let stack_offset = input_slot_to_imm_or_reg_2(inputs[0], func, &allocations);
-
-                            let r_out = allocations[&Value::InstructionOutput {
-                                instruction_index: *instruction_index,
-                                output_index: 0,
-                                block_index,
-                                data_type: outputs[0].tp,
-                            }];
-
-                            if let InputSlot::Constant(Constant::DataType(tp)) = inputs[1] {
-                                match (r_out, &stack_offset, tp) {
-                                    (Register::GPR(r_out), ConstOrReg::U64(offset), DataType::U32) => {
-                                        dynasm!(ops
-                                            ; ldr W(r_out as u32), [sp, func.get_stack_offset_for_location(*offset, DataType::U32)]
-                                        )
-                                    },
-                                    _ => todo!("Unsupported LoadFromStack operation: load {} from offset {:?} with datatype {}", r_out, stack_offset, tp),
-                                }
-                            }
-                        },
+                        crate::ir::InstructionType::WritePtr => compile_write_ptr(&mut ops, func, &allocations, inputs),
+                        crate::ir::InstructionType::SpillToStack => compile_spill_to_stack(&mut ops, func, &allocations, inputs),
+                        crate::ir::InstructionType::LoadFromStack => compile_load_from_stack(&mut ops, func, &allocations, inputs, outputs, output_registers),
                     }
                 },
-                crate::ir::Instruction::Branch { cond, if_true, if_false } => todo!("branch"),
+                crate::ir::Instruction::Branch { .. } => todo!("branch"),
                 crate::ir::Instruction::Jump { target } => {
                     if target.arguments.len() > 0 {
                         todo!("Jump with arguments");
@@ -259,7 +254,7 @@ pub fn compile(func: &mut IRFunction) {
                         );
                     }
                 },
-                crate::ir::Instruction::Return { value } => println!("return"),
+                crate::ir::Instruction::Return { .. } => println!("return"),
             }
         }
     }
