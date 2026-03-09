0c77027f727d635311a2d019351cfbd9d0344b07
Dillon Beliveau <dillon@dgb.sh>
2026-03-08 17:46:47 -0700

    nixfmt --strict flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,31 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShell {
-          buildInputs = with pkgs.${system}; [ cargo cargo-outdated rustc rust-analyzer rustfmt lldb bacon cargo-nextest ];
+          buildInputs = with pkgs.${system}; [
+            cargo
+            cargo-outdated
+            rustc
+            rust-analyzer
+            rustfmt
+            lldb
+            bacon
+            cargo-nextest
+          ];
         };
       });
     };
